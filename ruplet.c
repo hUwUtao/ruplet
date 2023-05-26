5fc56cdaabf1ce61466816eb6e816367d04467a9
hUwUtao <root@huwutao.me>
2023-05-26 11:47:29 +0700

    init

---
+++ ruplet.c
@@ -0,0 +1,20 @@
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+int main() {
+  unsigned char buf[2], obuf[1024], loc;
+
+  char *key = "this is encryption key";
+  unsigned int kl = strlen(key), ol = 1;
+
+  while (read(0, buf, sizeof(buf)) > 0) {
+    char toc = buf[0], ck = key[ol % kl];
+    toc = toc ^ loc ^ ck;
+    putchar(toc);
+    ol++;
+  }
+  return 0;
+}
