109e1865689a9196396e9ea5a65a8bfdea092bd9
hUwUtao <root@huwutao.me>
2023-05-26 19:33:24 +0700

    fix: no longer basic xor bcof typo

--- ruplet.c
+++ ruplet.c
@@ -1,19 +1,18 @@
 #include <stddef.h>
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 
 int main() {
-  unsigned char buf[1], obuf[1024], loc;
-
+  unsigned char buf[1], // write buffer to
+      lbuf = 0;              // iter chain
   char *key = "this is encryption key";
+  int enc = 1 > 0;
   unsigned int kl = strlen(key), ol = 1;
-
-  while (read(0, buf, sizeof(buf)) > 0) {
-    char toc = buf[0], ck = key[ol % kl];
-    toc = ck ^ loc ^ toc;
-    putchar(toc);
+  while (read(0, buf, 1) > 0) {
+    char o = key[ol % kl] ^ lbuf ^ buf[0];
+    lbuf = enc ? buf[0] : o;
+    putchar(o);
     ol++;
   }
   return 0;
